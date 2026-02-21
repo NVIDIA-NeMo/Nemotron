16b0bb2aac9bba0de9c970d2d86a36dc49574266
Charlie Truong <chtruong@nvidia.com>
2026-02-21 16:16:40 -0600

    Increase allowed line-length to 119
    Signed-off-by: Charlie Truong <chtruong@nvidia.com>

--- docs/conf.py
+++ docs/conf.py
@@ -23,12 +23,12 @@
 import os
 
 project = "Nemotron"
-copyright = "2025, NVIDIA Corporation"
+copyright = "2026, NVIDIA Corporation"
 author = "NVIDIA Corporation"
 
 # Version is set by CI via DOCS_VERSION env var (dev or stable)
 # Defaults to "dev" for local builds
-release = os.environ.get("DOCS_VERSION", "dev")
+release = os.environ.get("DOCS_VERSION", "nightly")
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
